f21a178f9aa0c27a31e87cec494bc4ed5592606c
grahbari <guillaume.rahbari@amadeus.com>
2016-05-30 16:49:31 +0200

    Build prod

--- app/templates/gulp/tasks/prod/bundles.app.prod.js
+++ app/templates/gulp/tasks/prod/bundles.app.prod.js
@@ -14,57 +14,11 @@ const BUNDLER_OPTIONS = {
     mangle: false
 };
 
-const SYSTEM_BUILDER_CONFIG = {
-    defaultJSExtensions: true,
-    paths              : {
-        [path.join(TMP_DIR, '*')]: path.join(TMP_DIR, '*'),
-        '*'                      : 'node_modules/*'
-    },
-    packages           : {
-        '@angular/core'                    : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/compiler'                : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/common'                  : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/http'                    : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/platform-browser'        : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/platform-browser-dynamic': {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/router-deprecated'       : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        '@angular/router'                  : {
-            main            : 'index.js',
-            defaultExtension: 'js'
-        },
-        'rxjs'                             : {
-            defaultExtension: 'js'
-        }
-    }
-};
-
 gulp.task('bundles.app.prod', (done) => {
-    //let builder = new Builder(SYSTEM_BUILDER_CONFIG);
     let builder = new Builder('./', path.join(SRC_DIR, 'systemjs.config.js'));
 
-    builder.buildStatic(path.join(TMP_DIR, 'app', 'main'),
-        path.join(JS_PROD_DIR, JS_PROD_APP_BUNDLE),
-        BUNDLER_OPTIONS)
-           .then(() => done());
+builder.buildStatic(path.join(TMP_DIR, 'app', 'main'),
+    path.join(JS_PROD_DIR, JS_PROD_APP_BUNDLE),
+    BUNDLER_OPTIONS)
+       .then(() => done());
 });
