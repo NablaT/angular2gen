9deefb3f55d0e5b4e8ca03a3c3f8cba2a557c623
NablaT <remi.pourtier@gmail.com>
2016-06-08 14:08:30 +0200

    Starting refactoring component generation

--- component/index.js
+++ component/index.js
@@ -18,39 +18,43 @@ module.exports = generators.Base.extend({
      */
     constructor: function () {
         generators.Base.apply(this, arguments);
+        this.hasArgs=true;
+        if(this.arguments.length>0){
+            console.log('has args');
+        }
+        else{
+            console.log("hasn't args");
+            this.args=false;
+        }
+        console.log("arguments: "+this.arguments);
         this.reworkArguments = lodash.camelCase(this.arguments);
-        this.nameOfComponent= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
+        this.nameOfComponent = this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
     },
 
     /**
      * Function checkSass. This function reads the file _package.json to know if user asked to install sass.
      */
     checkSass: function () {
-        this.hasSass=false;
-        var jsonContent=json.readFileSync("./package.json", 'utf8');
-        var storeJson= JSON.parse(jsonContent);
-        for(var currentKey in storeJson.dependencies) {
-            if(currentKey=="gulp-sass"){
-                this.hasSass=true;
+        this.hasSass = false;
+        var jsonContent = json.readFileSync("./package.json", 'utf8');
+        var storeJson = JSON.parse(jsonContent);
+        for (var currentKey in storeJson.dependencies) {
+            if (currentKey == "gulp-sass") {
+                this.hasSass = true;
             }
         }
     },
 
     /**
      * Function writing. This function copies the basic templates for components.
-     */
+
     writing: function () {
         this.argsInKebab = lodash.kebabCase(this.arguments);
-        this.basicTemplate = 'src/components/' + this.argsInKebab + '/' + this.argsInKebab;
+        this.basicTemplate = 'src/app/components/' + this.argsInKebab + '/' + this.argsInKebab;
         this.copy('components/_basic-template.html', this.basicTemplate + '.component.html');
         this.copy('components/_basic-template.ts', this.basicTemplate + '.component.ts');
-        if(this.hasSass){
-            this.copy('components/_basic-template.css', this.basicTemplate + '.component.scss');
-        }
-        else{
-            this.copy('components/_basic-template.css', this.basicTemplate + '.component.css');
-        }
+        this.copy('components/_basic-template.css', this.basicTemplate + '.component.scss');
         this.copy('components/_basic-template-test.ts', this.basicTemplate + '.component.spec.ts');
-    },
+    },*/
 
 });
