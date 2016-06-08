a505707a2b003ef1a8b5a7c208c32c49b13e70f9
NablaT <remi.pourtier@gmail.com>
2016-06-08 11:31:35 +0200

    Refactoring project regeneration with new structure

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var lodash = require('lodash');
 
 module.exports = generators.Base.extend({
 
+
     /**
      * Function askProjectName asks user the project name.
      * We store the answer in the attribute projectTitle.
@@ -17,6 +18,7 @@ module.exports = generators.Base.extend({
             default: "MyApp"
         }, function (answers) {
             this.projectTitle = answers.appName;
+            this.ENV = 'dev';
             done();
         }.bind(this));
     },
@@ -24,8 +26,8 @@ module.exports = generators.Base.extend({
     /**
      * Function askForSass. This function asks to user if he would like to use Sass.
      * We store his answers in the variable sassValue.
-     */
-    askForSass: function () {
+
+     askForSass: function () {
         var done = this.async();
         this.prompt({
             type: 'input',
@@ -37,7 +39,7 @@ module.exports = generators.Base.extend({
             this.sassValue = answers.sass.toLowerCase();
             done();
         }.bind(this));
-    },
+    },*/
 
     /**
      * Function askForBootstrap. This function asks to user if he would like to use Bootstrap.
@@ -118,120 +120,87 @@ module.exports = generators.Base.extend({
 
         this.copy('src/assets/picture.png', 'src/assets/picture.png');
         //Component folder creation
-        this.copy('src/components/_README.md', 'src/components/README.md');
-        var component=["app","about","header", "home"];
-        for(var i=0;i<component.length;i++){
-            this.copy('src/components/'+component[i]+'/_'+component[i]+'.component.html', 'src/components/'+component[i]+'/'+component[i]+'.component.html');
-            if (this.sassValue=== "yes" || this.sassValue=== "y") {
-                this.copy('src/components/'+component[i]+'/_'+component[i]+'.component.scss', 'src/components/'+component[i]+'/'+component[i]+'.component.scss');
-            }
-            else{
-                this.copy('src/components/app/_app.component.css', 'src/components/'+component[i]+'/'+component[i]+'.component.css');
-            }
-            this.copy('src/components/'+component[i]+'/_'+component[i]+'.component.ts', 'src/components/'+component[i]+'/'+component[i]+'.component.ts');
-            this.copy('src/components/'+component[i]+'/_'+component[i]+'.component.spec.ts', 'src/components/'+component[i]+'/'+component[i]+'.component.spec.ts');
-
+        this.copy('src/app/components/_README.md', 'src/app/components/README.md');
+        this.copy('src/app/_app.component.ts', 'src/app/app.component.ts');
+        this.copy('src/app/_app.component.spec.ts', 'src/app/app.component.spec.ts');
+        this.copy('src/app/_app.component.html', 'src/app/app.component.html');
+        this.copy('src/app/_app.component.scss', 'src/app/app.component.scss');
+        var component = ["about", "header", "home"];
+        var folderName = ["+about", "header", "+home"];
+        for (var i = 0; i < component.length; i++) {
+            this.copy('src/app/components/' + folderName[i] + '/_' + component[i] + '.component.html', 'src/app/components/' + folderName[i] + '/' + component[i] + '.component.html');
+            this.copy('src/app/components/' + folderName[i] + '/_' + component[i] + '.component.scss', 'src/app/components/' + folderName[i] + '/' + component[i] + '.component.scss');
+            this.copy('src/app/components/' + folderName[i] + '/_' + component[i] + '.component.ts', 'src/app/components/' + folderName[i] + '/' + component[i] + '.component.ts');
+            this.copy('src/app/components/' + folderName[i] + '/_' + component[i] + '.component.spec.ts', 'src/app/components/' + folderName[i] + '/' + component[i] + '.component.spec.ts');
         }
 
-        this.copy('src/shared/_README.md', 'src/shared/README.md');
-
-        //Directives folders and content creation
-        this.copy('src/shared/directives/_README.md', 'src/shared/directives/README.md');
-        this.copy('src/shared/directives/src/_README.md', 'src/shared/directives/src/README.md');
-        this.copy('src/shared/directives/test/_README.md', 'src/shared/directives/test/README.md');
-
-        //Services folders and content creation
-        this.copy('src/shared/services/_README.md', 'src/shared/services/README.md');
-        this.copy('src/shared/services/src/_README.md', 'src/shared/services/src/README.md');
-        this.copy('src/shared/services/test/_README.md', 'src/shared/services/test/README.md');
+        this.directory('src/app/shared', 'src/app/shared');
 
         //Styles folder and content creation
         //We initialise the message which appears in the readme of the style folder. We give two different message
         //if sass has been installed or not.
-        this.messageInReadMe="";
-        if (this.sassValue=== "yes" || this.sassValue=== "y") {
-            this.messageInReadMe="Initially, we generate two files: " +
+        this.messageInReadMe = "";
+        if (this.sassValue === "yes" || this.sassValue === "y") {
+            this.messageInReadMe = "Initially, we generate two files: " +
                 "- main.scss: File Sass which defines the common part in the design of the application" +
                 "- variables.scss: Contains all css variables used for the design";
 
-            if(this.bootstrapValue === "y" || this.bootstrapValue === "yes"){
-                if(this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes"){
-                    this.copy('src/shared/styles/_main-bootstrap-font.scss', 'src/shared/styles/main.scss');
+            if (this.bootstrapValue === "y" || this.bootstrapValue === "yes") {
+                if (this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes") {
+                    this.copy('src/styles/_main-bootstrap-font.scss', 'src/styles/main.scss');
                 }
-                else{
-                    this.copy('src/shared/styles/_main-bootstrap.scss', 'src/shared/styles/main.scss');
+                else {
+                    this.copy('src/styles/_main-bootstrap.scss', 'src/styles/main.scss');
                 }
             }
-            else if(this.foundationValue === "y" || this.foundationValue === "yes"){
-                if(this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes"){
-                    this.copy('src/shared/styles/_main-foundation-font.scss', 'src/shared/styles/main.scss');
+            else if (this.foundationValue === "y" || this.foundationValue === "yes") {
+                if (this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes") {
+                    this.copy('src/styles/_main-foundation-font.scss', 'src/styles/main.scss');
                 }
-                else{
-                    this.copy('src/shared/styles/_main-foundation.scss', 'src/shared/styles/main.scss');
+                else {
+                    this.copy('sr/styles/_main-foundation.scss', 'src/styles/main.scss');
                 }
-                this.copy('src/components/home/_home-foundation.component.html', 'src/components/home/home.component.html');
-                this.copy('src/components/home/_home-foundation.component.scss', 'src/components/home/home.component.scss');
-                this.copy('src/components/header/_header-foundation.component.html', 'src/components/header/header.component.html');
+                this.copy('src/app/components/home/_home-foundation.component.html', 'src/app/components/home/home.component.html');
+                this.copy('src/app/components/home/_home-foundation.component.scss', 'src/app/components/home/home.component.scss');
+                this.copy('src/app/components/header/_header-foundation.component.html', 'src/app/components/header/header.component.html');
             }
-            else{
-                this.copy('src/shared/styles/_main.scss', 'src/shared/styles/main.scss');
+            else {
+                this.copy('src/styles/_main.scss', 'src/styles/main.scss');
             }
-            this.copy('src/shared/styles/_variables.scss', 'src/shared/styles/_variables.scss');
+            this.copy('src/styles/_variables.scss', 'src/styles/_variables.scss');
         }
-        else{
-            //TODO: Add global css if user didn't choose Sass
-        }
-        this.copy('src/shared/styles/_README.md', 'src/shared/styles/README.md');
-        this.fontAwesomeContentSCSS="bondour";
+        this.copy('src/styles/_README.md', 'src/styles/README.md');
+
         //Copy grunt tasks
-        this.copy('gulp/README.md', 'gulp/README.md');
-        this.copy('gulp/gulp.conf.ts', 'gulp/gulp.conf.ts');
-        this.copy('gulp/browsersync.ts', 'gulp/browsersync.ts');
-        this.copy('gulp/tasks/gulp-clean.ts', 'gulp/tasks/gulp-clean.ts');
-        this.copy('gulp/tasks/gulp-copy.ts', 'gulp/tasks/gulp-copy.ts');
-        this.copy('gulp/tasks/gulp-inject.ts', 'gulp/tasks/gulp-inject.ts');
-        this.copy('gulp/tasks/gulp-sass.ts', 'gulp/tasks/gulp-sass.ts');
-        this.copy('gulp/tasks/gulp-serve.ts', 'gulp/tasks/gulp-serve.ts');
-        this.copy('gulp/tasks/gulp-typescript.ts', 'gulp/tasks/gulp-typescript.ts');
-        this.copy('gulp/tasks/gulp-watch.ts', 'gulp/tasks/gulp-watch.ts');
-
-        //Manual typings folder
-        this.copy('manual_typings/README.md', 'manual_typings/README.md');
-        this.copy('manual_typings/connect-history-api-fallback.d.ts', 'manual_typings/connect-history-api-fallback.d.ts');
-        this.copy('manual_typings/require-dir.d.ts', 'manual_typings/require-dir.d.ts');
-        this.copy('manual_typings/manual-typings.d.ts', 'manual_typings/manual-typings.d.ts');
-
-
+        this.directory("gulp", "gulp");
     },
 
     /**
      * Function installSass. This function checks if user want to install Sass, if yes it runs the installation.
      */
-    installSass: function(){
-        if (this.sassValue=== "yes" || this.sassValue=== "y") {
-            this.npmInstall(['gulp-sass'], { 'save': true }); //npm install gulp-sass --save
-        }
+    installSass: function () {
+        this.npmInstall(['gulp-sass'], {'save': true}); //npm install gulp-sass --save
     },
 
     /**
      * Function installBootstrapOrFoundation. This function checks if user want to install Bootstrap or Foundation.
      * If one of those framework has been chosen, the function install it.
      */
-    installBootstrapOrFoundation: function(){
+    installBootstrapOrFoundation: function () {
         if (this.bootstrapValue === "y" || this.bootstrapValue === "yes") {
-            this.npmInstall(['bootstrap@4.0.0-alpha.2'], { 'save': true }); //npm install bootstrap@4.0.0-alpha.2 --save
+            this.npmInstall(['bootstrap@4.0.0-alpha.2'], {'save': true}); //npm install bootstrap@4.0.0-alpha.2 --save
         }
-        else if (this.foundationValue === "y" || this.foundationValue === "yes" ) {
-            this.npmInstall(['foundation-sites'], { 'save': true }); //npm install foundation-sites --save
+        else if (this.foundationValue === "y" || this.foundationValue === "yes") {
+            this.npmInstall(['foundation-sites'], {'save': true}); //npm install foundation-sites --save
         }
     },
 
     /**
      * Function installFontAwesome. This function checks if user want to install FontAwesome, if yes it runs the installation.
      */
-    installFontAwesome: function(){
-        if (this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes" ) {
-            this.npmInstall(['font-awesome'], { 'save': true }); //npm install font-awesome --save
+    installFontAwesome: function () {
+        if (this.fontAwesomeValue === "y" || this.fontAwesomeValue === "yes") {
+            this.npmInstall(['font-awesome'], {'save': true}); //npm install font-awesome --save
         }
     },
 
@@ -239,12 +208,12 @@ module.exports = generators.Base.extend({
      * Function install. This function installs all dependencies according to user choices.
      */
     install: function () {
-        this.npmInstall(['gulp-cli'], { 'g': true });
+        this.npmInstall(['gulp-cli'], {'g': true});
         this.npmInstall(); //npm install
         //this.spawnCommand('gulp', ['serve']);
     },
 
     /*runApp: function(){
-        this.spawnCommand('gulp', ['serve']);
-    }*/
+     this.spawnCommand('gulp', ['serve']);
+     }*/
 });
