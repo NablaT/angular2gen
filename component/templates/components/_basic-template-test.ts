3bec946ee07bcb8071a688ba9d1b418a05d4e70c
NablaT <remi.pourtier@laposte.net>
2016-06-11 09:20:00 +0200

    Fixing bug in generation component in the test file

--- component/templates/components/_basic-template-test.ts
+++ component/templates/components/_basic-template-test.ts
@@ -1,7 +1,7 @@
 /**
  * Test Component <%= nameOfComponent %>
  */
-import {<%= nameOfComponent %>Component} from "./<%= argsInKebab %>.component";
+import {<%= componentName %>Component} from "./<%= argsInKebab %>.component";
 import {Component} from "@angular/core";
 @Component({
     selector: 'test-<%= argsInKebab %>',
