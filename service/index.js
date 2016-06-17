0237b413a3b9c319e66c3455c96d41abf38f052f
NablaT <remi.pourtier@gmail.com>
2016-06-17 13:56:32 +0200

    Adding path possibility into service generation process

--- service/index.js
+++ service/index.js
@@ -3,6 +3,8 @@
  */
 var generators = require('yeoman-generator');
 var lodash = require('lodash');
+var common = require('../common-function');
+
 
 module.exports = generators.Base.extend({
 
@@ -11,6 +13,11 @@ module.exports = generators.Base.extend({
      */
     constructor: function () {
         generators.Base.apply(this, arguments);
+        this.hasArgs = true;
+
+        this.argsArray = common.getArgsInArray(this.arguments);
+        if(this.argsArray.length == 0) this.hasArgs=false;
+
         this.reworkArguments = lodash.camelCase(this.arguments);
         this.nameOfService= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
     },
@@ -19,13 +26,36 @@ module.exports = generators.Base.extend({
      * Function writing. This function copies the basic templates for services.
      */
     writing: function () {
+        if (this.hasArgs) {
+            if (this.argsArray.length > 1) {
+                this.nameOfService=this.argsArray[this.argsArray.length-1];
+                var pathAndArgs=common.getPathAndArgs(this.argsArray, false);
+                this.path= pathAndArgs[0];
+                this.argsInKebab = pathAndArgs[1];
+
+                this.basicTemplateSrc = 'src/app/shared/services/src/' + this.path + "/" + this.argsInKebab;
+                this.basicTemplateTest = 'src/app/shared/services/test/' + this.path + "/" + this.argsInKebab;
+                this.pathForImport ='../../src/'+ this.path+"/"+this.argsInKebab;
+            }
+            else{
+                this.nameOfService=this.arguments;
+                this.argsInKebab = lodash.kebabCase(this.arguments);
+
+                this.basicTemplateSrc = 'src/app/shared/services/src/' +  this.argsInKebab;
+                this.basicTemplateTest = 'src/app/shared/services/test/' +  this.argsInKebab;
+                this.pathForImport = '../src/'+this.argsInKebab;
+            }
+            this.copy('services/_basic-template.ts', this.basicTemplateSrc + '.service.ts');
+            this.copy('services/_basic-template-test.ts', this.basicTemplateTest + '.service.spec.ts');
+        }
+        /**
         this.argsInKebab = lodash.kebabCase(this.arguments);
 
         this.basicTemplateSrc = 'src/app/shared/services/src/' + this.argsInKebab;
         this.basicTemplateTest = 'src/app/shared/services/test/' + this.argsInKebab;
 
         this.copy('services/_basic-template.ts', this.basicTemplateSrc + '.service.ts');
-        this.copy('services/_basic-template-test.ts', this.basicTemplateTest + '.service.spec.ts');
+        this.copy('services/_basic-template-test.ts', this.basicTemplateTest + '.service.spec.ts');**/
     },
 
 });
