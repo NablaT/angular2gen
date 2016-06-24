77453c7cb74f7bcec73de73d4c1fbfc068d51ab2
NablaT <remi.pourtier@gmail.com>
2016-06-24 16:52:00 +0200

    starting model generation

--- model/index.js
+++ model/index.js
@@ -18,34 +18,34 @@ module.exports = generators.Base.extend({
         if(this.argsArray.length == 0) this.hasArgs=false;
 
         this.reworkArguments = lodash.camelCase(this.arguments);
-        this.nameOfDirective= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
+        this.nameOfModel= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
     },
 
     /**
-     * Function writing. This function copies the basic templates for directives.
+     * Function writing. This function copies the basic templates for models.
      */
     writing: function () {
         if (this.hasArgs) {
             if (this.argsArray.length > 1) {
-                this.nameOfDirective=this.argsArray[this.argsArray.length-1];
+                this.nameOfModel=this.argsArray[this.argsArray.length-1];
                 var pathAndArgs=common.getPathAndArgs(this.argsArray, false);
                 this.path= pathAndArgs[0];
                 this.argsInKebab = pathAndArgs[1];
 
-                this.basicTemplateSrc = 'src/app/shared/directives/src/' + this.path + "/" + this.argsInKebab;
-                this.basicTemplateTest = 'src/app/shared/directives/test/' + this.path + "/" + this.argsInKebab;
+                this.basicTemplateSrc = 'src/app/shared/models/src/' + this.path + "/" + this.argsInKebab;
+                this.basicTemplateTest = 'src/app/shared/models/test/' + this.path + "/" + this.argsInKebab;
                 this.pathForImport ='../../src/'+ this.path+"/"+this.argsInKebab;
             }
             else{
-                this.nameOfDirective=this.arguments;
+                this.nameOfModel=this.arguments;
                 this.argsInKebab = lodash.kebabCase(this.arguments);
 
-                this.basicTemplateSrc = 'src/app/shared/directives/src/' +  this.argsInKebab;
-                this.basicTemplateTest = 'src/app/shared/directives/test/' +  this.argsInKebab;
+                this.basicTemplateSrc = 'src/app/shared/models/src/' +  this.argsInKebab;
+                this.basicTemplateTest = 'src/app/shared/models/test/' +  this.argsInKebab;
                 this.pathForImport = '../src/'+this.argsInKebab;
             }
-            this.copy('directives/_basic-template.ts', this.basicTemplateSrc + '.directive.ts');
-            this.copy('directives/_basic-template-test.ts', this.basicTemplateTest + '.directive.spec.ts');
+            this.copy('models/_basic-template.ts', this.basicTemplateSrc + '.model.ts');
+            this.copy('models/_basic-template-test.ts', this.basicTemplateTest + '.model.spec.ts');
         }
 
     },
---
+++ model/template/models/_basic-template.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by rpourtier on 24/06/2016.
+ */
