9275a4de5eaf6351fa3e601270f648511427eb90
NablaT <remi.pourtier@gmail.com>
2016-09-15 12:04:56 +0200

    Doc for main

--- app/templates/src/app/_main.ts
+++ app/templates/src/app/_main.ts
@@ -1,12 +1,22 @@
+/**
+ * The browser platform without a compiler
+ */
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {enableProdMode} from "@angular/core";
+
+/**
+ * The app module
+ */
 import {AppModule} from "./app.module";
 
-let ENV : string = '<%%= ENV %>';
-let PROD : string = 'prod';
+let ENV:string = '<%%= ENV %>';
+let PROD:string = 'prod';
 if (ENV === PROD) {
     enableProdMode();
 }
 
+/**
+ * Compile and launch the module
+ */
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
