9c7fb72dd2648d49dfa4d250a8800e9346c8c608
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2016-09-17 17:53:48 +0200

    repair tests

--- app/templates/src/app/_app.component.spec.ts
+++ app/templates/src/app/_app.component.spec.ts
@@ -1,3 +1,43 @@
-export function main () {
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Route } from '@angular/router';
 
-}
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/+home/home.component';
+import { AboutComponent } from './components/+about/about.component';
+
+let comp:    AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+let el:      DebugElement;
+
+describe('AppComponent', () => {
+
+    let config: Route[] = [
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent }
+    ];
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule.withRoutes(config)],
+            declarations: [ AppComponent, HomeComponent, AboutComponent ],
+        })
+        .compileComponents() // compile template and css
+        .then( 
+            () => {
+                // create component and test fixture
+                fixture = TestBed.createComponent(AppComponent);
+
+                // get test component from the fixture
+                comp = fixture.componentInstance;
+            }
+        );
+    }));
+
+    it('can instantiate it', () => {
+        expect(comp).not.toBeNull();
+    });
+
+});
