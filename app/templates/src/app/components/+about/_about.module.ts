73db397614e3b2f99ca1957a80d1262f002e8962
NablaT <remi.pourtier@gmail.com>
2016-09-19 16:53:07 +0200

    Bug fix: Add import statement for CardComponent into AboutModule

--- app/templates/src/app/components/+about/_about.module.ts
+++ app/templates/src/app/components/+about/_about.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
+import { CardComponent } from "../core/card/card.component";
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [AboutComponent],
+    declarations: [AboutComponent, CardComponent],
     exports: [AboutComponent]
 })
 
